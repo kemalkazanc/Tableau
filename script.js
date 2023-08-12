731464bdb9dd7a5d3c71cd51604d549cf28a3fa3
Kemal Kazanc <keemalkazanc@gmail.com>
2023-08-12 15:51:14 +0200

    mise en place un petit tableau

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnValider = document.getElementsByClassName("btn1")[0];
+console.log(btnValider);
+
+btnValider.addEventListener("click", () => {
+  document.body.style.backgroundColor = "green";
+});
